92fe69581f02690f42a6cc5b5b8c1333c36b91fb
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2012-09-10 17:58:44 +0200

    add popup menu

---
+++ src/nvPopupMenu.c
@@ -0,0 +1,65 @@
+#include<cgenerics/cgRectangle.h>
+#include"nvPopupMenu.h"
+
+static nvPopupMenu *nvPopupMenu__new_(int x, int y);
+
+static nvPopupMenu *nvPopupMenu__new_(int x, int y) {
+    nvPopupMenu *this = malloc(sizeof(*this));
+
+    if (this != NULL) {
+        this->window = nvWindow__new(x, y, 0, 0);
+        this->menu = nvVerticalMenu__new(0, 0);
+        nvWindow_addWidget(this->window, this->menu);
+    } else
+        cgAppState_THROW(cgAppState__getInstance(), Severity_fatal, cgExceptionID_CannotAllocate, "cannot allocate nvPopupMenu");
+    return this;
+}
+
+nvWidget *nvPopupMenu__new(int x, int y) {
+    nvPopupMenu *menu = nvPopupMenu__new_(x, y);
+
+    nvWidget *this = nvWidget__new_(nvWidgetType_popupMenu, x, y, 0, 0, (void *)menu);
+    nvWidget_setRefreshMethod(this, nvPopupMenu_refresh);
+    nvWidget_setReceiveKeyMethod(this, nvPopupMenu_receiveKey);
+    nvWidget_setDoesOverlapClientRectMethod(this, nvPopupMenu_doesOverlapClientRect);
+    nvWidget_setFocusMethod(this, nvPopupMenu_focus);
+    nvWidget_setDefocusMethod(this, nvPopupMenu_deFocus);
+
+    return this;
+}
+
+void nvPopupMenu_delete(nvWidget * this) {
+    nvWindow_delete(THIS(nvPopupMenu)->window);
+    nvVerticalMenu_delete(THIS(nvPopupMenu)->menu);
+    nvWidget_delete_(this);
+}
+
+void nvPopupMenu_addWidget(nvWidget * this, nvWidget* widget) {
+    nvVerticalMenu_addWidget(THIS(nvPopupMenu)->menu, widget);
+    nvWindow_pack(THIS(nvPopupMenu)->window);
+}
+
+bool nvPopupMenu_receiveKey(nvWidget * this, int ch) {
+    bool rv = nvWidget_receiveKey(THIS(nvPopupMenu)->window, ch);
+    /* TODO handle remaining keys? */
+    return rv;
+}
+
+void nvPopupMenu_refresh(nvWidget * this) {
+    if (cgArray_getSize(nvWidget, nvSubwidgetManager_getSubWidgets(nvVerticalMenu_getSubwidgetManager(THIS(nvPopupMenu)->menu))) > 0) {
+        nvWidget_refresh(THIS(nvPopupMenu)->window);
+    }
+}
+
+bool nvPopupMenu_doesOverlapClientRect(nvWidget* this, nvWidget* that) {
+    return nvWidget_doesOverlapClientRect(THIS(nvPopupMenu)->window, that);
+}
+
+bool nvPopupMenu_focus(nvWidget* this) {
+    return nvWidget_focus(THIS(nvPopupMenu)->window);
+}
+
+bool nvPopupMenu_deFocus(nvWidget* this) {
+    return nvWidget_deFocus(THIS(nvPopupMenu)->window);
+}
+
---
+++ src/nvPopupMenu.h
@@ -0,0 +1,29 @@
+
+#ifndef _NV_POPUP_MENU_H
+#define _NV_POPUP_MENU_H
+
+#include"nvwm.h"
+#include"nvWindow.h"
+#include"nvVerticalMenu.h"
+
+typedef struct {
+    nvWidget *window;
+    nvWidget *menu;
+} nvPopupMenu;
+
+nvWidget *nvPopupMenu__new(int x, int y);
+
+void nvPopupMenu_delete(nvWidget * this);
+
+void nvPopupMenu_addWidget(nvWidget * this, nvWidget * widget);
+
+bool nvPopupMenu_receiveKey(nvWidget * this, int ch);
+
+void nvPopupMenu_refresh(nvWidget * this);
+
+bool nvPopupMenu_doesOverlapClientRect(nvWidget * this, nvWidget * that);
+
+bool nvPopupMenu_focus(nvWidget * this);
+
+bool nvPopupMenu_deFocus(nvWidget * this);
+#endif
