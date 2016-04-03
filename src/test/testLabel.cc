1e343d33daed3b80a7fa1586810a1c1a34292a0c
Christian Friedl <xian@debisamson>
2016-04-03 12:53:11 +0200

    debug label

--- src/test/testLabel.cc
+++ src/test/testLabel.cc
@@ -1,9 +1,12 @@
 #include "Label.h"
+#include "Logger.h"
 
 using namespace nv;
 
 int main() {
     Label l("abcde", 1, 1);
+    Logger::get().log("label l:");
+    Logger::get().log(l.toString());
     l.refresh();
     getch();
 }
