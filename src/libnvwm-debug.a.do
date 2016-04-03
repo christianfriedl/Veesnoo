247f1fff4c70dc89e2e5576fe9db35f0c0a376da
Christian Friedl <mag.christian.friedl@gmail.com>
2016-04-03 13:20:46 +0200

    re-add textbox

--- src/libnvwm-debug.a.do
+++ src/libnvwm-debug.a.do
@@ -1,6 +1,6 @@
 OBJECTS="CursesException.o CursesManager.o CursesWindow.o Rect.o Widget.o Label.o"
 OBJECTS="$OBJECTS FocusManaging.o SingleFocusManager.o FocusableWidget.o Button.o"
-OBJECTS="$OBJECTS Checkbox.o"
+OBJECTS="$OBJECTS Checkbox.o TextBox.o"
 OBJECTS="$OBJECTS Logger.o"
 
 redo-ifchange $OBJECTS
--- src/test/config.sh
+++ src/test/config.sh
@@ -9,6 +9,6 @@ OBJECTS="testWidget.o testLabel.o testButton.o testCheckbox.o testHorizontalMenu
 OBJECTS="$OBJECTS testWindow.o testTextBox.o testPopupMenu.o"
 
 #### debugging
-BINARIES="testWidget testLabel testButton testCheckbox"
+BINARIES="testWidget testLabel testButton testCheckbox testTextBox"
 OBJECTS="testWidget.o testLabel.o testCheckbox.o"
-OBJECTS="$OBJECTS testButton.o "
+OBJECTS="$OBJECTS testButton.o testTextBox.o"
