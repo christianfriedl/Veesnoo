6bab3fa0af039a9f18a258f3954846b8c53b91bb
Christian Friedl <cf@christian-friedl.info>
2017-02-26 18:36:40 +0100

    add test

---
+++ src/test/testHorizontalMenu_hardcoded.cc
@@ -0,0 +1,44 @@
+#include <memory>
+#include "Widget.h"
+#include "MenuItem.h"
+#include "HorizontalMenu.h"
+
+using namespace nv;
+
+int main() {
+    auto m1 (std::make_shared<MenuItem>("menuitem1"));
+    auto m2 (std::make_shared<MenuItem>("menuitem2"));
+    auto m3 (std::make_shared<MenuItem>("menuitem3"));
+    HorizontalMenu hm(6, 2);
+    hm.addWidget(m1);
+    hm.addWidget(m2);
+    hm.addWidget(m3);
+
+    hm.refresh();
+    getch(); // refresh, no highligts
+    hm.focus();
+    hm.refresh();
+    getch(); // refresh, first item is focused and highlighted
+    hm.receiveKey(' ');
+    hm.refresh();
+    getch(); // r4fresh, next item is focused and highlighted
+    hm.receiveKey(' ');
+    hm.refresh();
+    getch(); // r4fresh, next item is focused and highlighted
+    hm.receiveKey(' ');
+    hm.refresh();
+    getch(); // r4fresh, first item is focused and highlighted
+    hm.receiveKey(' ');
+    hm.refresh();
+    getch(); // r4fresh, next item is focused and highlighted
+    hm.receiveKey('h');
+    hm.refresh();
+    getch(); // r4fresh, first item is focused and highlighted
+    hm.receiveKey('h');
+    hm.refresh();
+    getch(); // r4fresh, last item is focused and highlighted
+
+    hm.focusThis(m2);
+    hm.refresh();
+    getch(); // r4fresh, 2nd item is focused and highlighted
+}
