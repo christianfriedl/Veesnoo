f079e2932399f1c28721daaebe3f3c00ef384768
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2017-03-02 14:23:19 +0100

    ignore now-autogenerated redo-config.sh

--- src/redo-config.sh
+++
@@ -1,8 +0,0 @@
-CC=clang++
-CURSESLIB=-lncursesw
-CFLAGS="-g -Wall -std=c++14"
-LDFLAGS="-lncursesw -std=c++14"
-AR="ar"
-ARFLAGS="rs"
-HAVEDEFINES="-DHAVE_USR_INCLUDE_NCURSESW_NCURSES_H "
-LIBOBJECTS=" Logger.o  CursesException.o CursesManager.o CursesWindow.o Rect.o Widget.o Label.o  Button.o FocusableWidget.o  TextBox.o Checkbox.o MenuItem.o  Container.o  HorizontalMenu.o VerticalMenu.o FocusableContainer.o ContainerFocusManaging.o ContainerFocusManager.o  Window.o PopupMenu.o  MainLoop.o "
