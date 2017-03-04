ade04a8c17aec66dd030656e450d9252be8c957c
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2017-03-04 19:41:19 +0100

    add universal header file

---
+++ src/nvwm.h
@@ -0,0 +1,19 @@
+#ifndef NVWM_H
+#define NVWM_H
+
+#include "Screen.h"
+#include "TextBox.h"
+#include "Label.h"
+#include "Button.h"
+#include "FocusableWidget.h"
+#include "TextBox.h"
+#include "Checkbox.h"
+#include "MenuItem.h"
+#include "HorizontalMenu.h"
+#include "VerticalMenu.h"
+#include "Window.h"
+#include "PopupMenu.h"
+#include "MainLoop.h"
+#include "CursesManager.h"
+
+#endif
