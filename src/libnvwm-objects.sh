27fcb4fa7d5d0a2d6406267fcb3bb843aaf5c818
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2017-03-22 15:52:45 +0100

    reorganize object list

--- src/libnvwm-objects.sh
+++ src/libnvwm-objects.sh
@@ -1,31 +1,86 @@
 LIBSOURCES=""
-LIBSOURCES="$LIBSOURCES Logger.cc "
-LIBSOURCES="$LIBSOURCES CursesException.cc CursesManager.cc CursesWindow.cc Rect.cc Widget.cc Label.cc "
-LIBSOURCES="$LIBSOURCES Button.cc FocusableWidget.cc "
-LIBSOURCES="$LIBSOURCES TextBox.cc Checkbox.cc MenuItem.cc "
-LIBSOURCES="$LIBSOURCES Container.cc "
-LIBSOURCES="$LIBSOURCES HorizontalMenu.cc VerticalMenu.cc FocusableContainer.cc ContainerFocusManaging.cc ContainerFocusManager.cc "
-LIBSOURCES="$LIBSOURCES Window.cc PopupMenu.cc "
-LIBSOURCES="$LIBSOURCES MainLoop.cc "
-LIBSOURCES="$LIBSOURCES Screen.cc "
-LIBSOURCES="$LIBSOURCES Event.cc BasicEvent.cc ChangeEvent.cc "
-LIBOBJECTS="$LIBOBJECTS MessageBox.cc "
-LIBOBJECTS="$LIBOBJECTS ConfirmBox.cc "
-LIBOBJECTS="$LIBOBJECTS RadioButton.cc "
-LIBOBJECTS="$LIBOBJECTS FocusStealer.cc "
-
 LIBOBJECTS=""
+
+LIBSOURCES="$LIBSOURCES Logger.cc "
 LIBOBJECTS="$LIBOBJECTS Logger.o "
-LIBOBJECTS="$LIBOBJECTS CursesException.o CursesManager.o CursesWindow.o Rect.o Widget.o Label.o "
-LIBOBJECTS="$LIBOBJECTS Button.o FocusableWidget.o "
-LIBOBJECTS="$LIBOBJECTS TextBox.o Checkbox.o MenuItem.o "
+
+LIBSOURCES="$LIBSOURCES Rect.cc "
+LIBOBJECTS="$LIBOBJECTS Rect.o "
+
+LIBSOURCES="$LIBSOURCES Event.cc "
+LIBOBJECTS="$LIBOBJECTS Event.o "
+
+LIBSOURCES="$LIBSOURCES BasicEvent.cc "
+LIBOBJECTS="$LIBOBJECTS BasicEvent.o "
+
+LIBSOURCES="$LIBSOURCES ChangeEvent.cc "
+LIBOBJECTS="$LIBOBJECTS ChangeEvent.o "
+
+LIBSOURCES="$LIBSOURCES CursesException.cc "
+LIBOBJECTS="$LIBOBJECTS CursesException.o "
+
+LIBSOURCES="$LIBSOURCES CursesManager.cc "
+LIBOBJECTS="$LIBOBJECTS CursesManager.o "
+
+LIBSOURCES="$LIBSOURCES CursesWindow.cc "
+LIBOBJECTS="$LIBOBJECTS CursesWindow.o "
+
+LIBSOURCES="$LIBSOURCES Widget.cc "
+LIBOBJECTS="$LIBOBJECTS Widget.o "
+
+LIBSOURCES="$LIBSOURCES FocusableWidget.cc "
+LIBOBJECTS="$LIBOBJECTS FocusableWidget.o "
+
+LIBSOURCES="$LIBSOURCES Container.cc "
 LIBOBJECTS="$LIBOBJECTS Container.o "
-LIBOBJECTS="$LIBOBJECTS HorizontalMenu.o VerticalMenu.o FocusableContainer.o ContainerFocusManaging.o ContainerFocusManager.o "
-LIBOBJECTS="$LIBOBJECTS Window.o PopupMenu.o "
-LIBOBJECTS="$LIBOBJECTS MainLoop.o "
-LIBOBJECTS="$LIBOBJECTS Screen.o "
-LIBOBJECTS="$LIBOBJECTS Event.o BasicEvent.o ChangeEvent.o "
-LIBOBJECTS="$LIBOBJECTS MessageBox.o "
-LIBOBJECTS="$LIBOBJECTS ConfirmBox.o "
+
+LIBSOURCES="$LIBSOURCES FocusableContainer.cc "
+LIBOBJECTS="$LIBOBJECTS FocusableContainer.o "
+
+LIBSOURCES="$LIBSOURCES ContainerFocusManager.cc "
+LIBOBJECTS="$LIBOBJECTS ContainerFocusManager.o "
+
+LIBSOURCES="$LIBSOURCES ContainerFocusManaging.cc "
+LIBOBJECTS="$LIBOBJECTS ContainerFocusManaging.o "
+
+LIBSOURCES="$LIBSOURCES Label.cc "
+LIBOBJECTS="$LIBOBJECTS Label.o "
+
+LIBSOURCES="$LIBSOURCES Button.cc "
+LIBOBJECTS="$LIBOBJECTS Button.o "
+
+LIBSOURCES="$LIBSOURCES TextBox.cc "
+LIBOBJECTS="$LIBOBJECTS TextBox.o "
+
+LIBSOURCES="$LIBSOURCES Checkbox.cc "
+LIBOBJECTS="$LIBOBJECTS Checkbox.o "
+
+LIBSOURCES="$LIBSOURCES MenuItem.cc "
+LIBOBJECTS="$LIBOBJECTS MenuItem.o "
+
+LIBSOURCES="$LIBSOURCES HorizontalMenu.cc "
+LIBOBJECTS="$LIBOBJECTS HorizontalMenu.o "
+
+LIBSOURCES="$LIBSOURCES VerticalMenu.cc "
+LIBOBJECTS="$LIBOBJECTS VerticalMenu.o "
+
+LIBSOURCES="$LIBSOURCES PopupMenu.cc "
+LIBOBJECTS="$LIBOBJECTS PopupMenu.o "
+
+LIBSOURCES="$LIBSOURCES RadioButton.cc "
 LIBOBJECTS="$LIBOBJECTS RadioButton.o "
-LIBOBJECTS="$LIBOBJECTS FocusStealer.o "
+
+LIBSOURCES="$LIBSOURCES Screen.cc "
+LIBOBJECTS="$LIBOBJECTS Screen.o "
+
+LIBSOURCES="$LIBSOURCES MessageBox.cc "
+LIBOBJECTS="$LIBOBJECTS MessageBox.o "
+
+LIBSOURCES="$LIBSOURCES ConfirmBox.cc "
+LIBOBJECTS="$LIBOBJECTS ConfirmBox.o "
+
+LIBSOURCES="$LIBSOURCES MainLoop.cc "
+LIBOBJECTS="$LIBOBJECTS MainLoop.o "
+
+LIBSOURCES="$LIBSOURCES Window.cc "
+LIBOBJECTS="$LIBOBJECTS Window.o "
